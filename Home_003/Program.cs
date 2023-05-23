6bf23ce10922fa03887dc37bb0ee756c6963ec9a
Serge Bogoduhov <zz9250347500@gmail.com>
2023-05-23 21:27:36 +0300

    got

--- Home_003/Program.cs
+++ Home_003/Program.cs
@@ -9,3 +9,10 @@
 // 123456 -> no
 
 
+t = int(input("Введите номер билета: "))
+l = int(t[0]) + int(t[1]) + int(t[2])
+r = int(t[3]) + int(t[4]) + int(t[5])
+if l == r:
+    print("Yes")
+else:
+    print("NO")
